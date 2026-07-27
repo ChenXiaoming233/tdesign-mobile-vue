677bc39ba859f3e82b3ed963a352d6a3507e9dcf
明 <xmdermail@gmail.com>
2026-07-27 19:19:32 +0800

    feat(tab-bar): retain full glass playground

--- src/tab-bar/__test__/demo.test.jsx
+++ src/tab-bar/__test__/demo.test.jsx
@@ -7,6 +7,7 @@ import badgePropsVue from '@/tab-bar/demos/badge-props.vue';
 import baseVue from '@/tab-bar/demos/base.vue';
 import customVue from '@/tab-bar/demos/custom.vue';
 import glassVue from '@/tab-bar/demos/glass.vue';
+import liquidGlassVue from '@/tab-bar/demos/liquid-glass.vue';
 import mobileVue from '@/tab-bar/demos/mobile.vue';
 import pureIconVue from '@/tab-bar/demos/pure-icon.vue';
 import roundVue from '@/tab-bar/demos/round.vue';
@@ -18,6 +19,7 @@ const mapper = {
   baseVue,
   customVue,
   glassVue,
+  liquidGlassVue,
   mobileVue,
   pureIconVue,
   roundVue,
